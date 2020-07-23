ed6938d3af1ce3200b7cccebef01a4195fbd07e9
Rafe <e-rbnunez@neoris.com>
2020-07-23 11:09:25 -0500

    upddate DefUtils

--- src/app/shared/utils/default.ts
+++ src/app/shared/utils/default.ts
@@ -1,9 +1,8 @@
-
 export class DefUtils {
     static getPathValue<T>(path: string, source: any, defaultValue: T = null): T {
         const pathArr: any[] = path.split('.');
-        const result = pathArr.reduce((xs, x) => (xs && (xs[x] || xs[x] === '')) ? xs[x] : null, source);
-        return result || defaultValue;
+        const result = pathArr.reduce((xs, x) => (xs && (xs[x] !== null && xs[x] !== '')) ? xs[x] : null, source);
+        return result !== null ? result : defaultValue;
     }
 
 }
